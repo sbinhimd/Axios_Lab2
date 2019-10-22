f0877c8e747f13181bdbc4bdde9e028757b1a42f
Suhaib Binhimd <sbinhimd@hotmail.com>
2019-10-22 16:17:32 +0300

    js completed

--- main.js
+++ main.js
@@ -4,6 +4,59 @@ axios({
 })
 .then((Response)=>{
     console.log(Response);
-    
+
+    var randomNum = Math.floor(Math.random()*9)
+    var backImg = Response.data[randomNum].urls.regular;
+    console.log(backImg);
+   
+   $('body').css('background-image', 'url(' + backImg + ')')
+
 })
 .catch()
+
+
+const icons = {
+    Clear: '☀',
+    Rain: '️🌧',
+    Storm: '⛈',
+    Snow: '🌨',
+    Mist: '🌫',
+    Clouds: '☁',
+  };
+
+axios({
+    method: 'get',
+    url: "http://api.openweathermap.org/data/2.5/weather?q=Riyadh&units=metric&APPID=fddde78406af0a2e6faa75a479923d3f"
+})
+.then((response)=> {
+    const city = response.data.name
+    const weather = response.data.main.temp
+    const weather1 = response.data.weather[0].main
+    console.log(response);
+    
+    $('.weather').append(`<h1>${icons[weather1.toString()]}</h1>`)
+    $('.weather').append(`<h1>${weather}</h1>`)
+    $('.weather').append(`<h1>${city}</h1>`)
+    
+})
+.catch((error)=>{})
+
+
+
+var time = moment().format('LTS');
+$('.time').append(`${time}`)
+
+// axios({
+//     method: 'get',
+//     url:'https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=js&lang=en'
+// })
+// .then((response)=> {
+//     var quote= response.quote
+//     console.log(quote);
+    
+// $('.quote').append(`${quote.quoteText} ${quote.quoteAuthor} `)
+// })
+// .catch((response) => {
+//    console.log("quote error");
+   
+// })
